f360c8476bf1c61e2e0a8e92f3fc8f2384d21dda
janissl <janis_slapins@inbox.lv>
2021-07-27 12:39:46 +0300

    Remove functionality implemented in PriceService, add functionality for object comparison

--- src/main/java/com/github/janissl/draftticket/model/Ticket.java
+++ src/main/java/com/github/janissl/draftticket/model/Ticket.java
@@ -3,14 +3,14 @@ package com.github.janissl.draftticket.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.Objects;
 
-@JsonIgnoreProperties(value = {"netPrice"})
+@JsonIgnoreProperties(value = {"basePrice"})
 public class Ticket {
     private final TicketType ticketType;
     private final Double basePrice;
     private final Integer count;
-    private BigDecimal price;
+    private BigDecimal totalPrice;
 
     public Ticket(TicketType ticketType, Double basePrice) {
         this(ticketType, basePrice, 1);
@@ -22,34 +22,16 @@ public class Ticket {
         this.count = numberOfItems;
     }
 
-    public void setTotalPrice(Double totalPrice) {
-        this.price = BigDecimal.valueOf(totalPrice).setScale(2, RoundingMode.HALF_UP);
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.totalPrice = price;
     }
 
     public BigDecimal getPrice() {
-        return price;
-    }
-
-    public Double getNetPrice() {
-        Double percentageDiscountChild = 50.0;
-        Double percentageLuggage = 30.0;
-        Double netPrice;
-
-        switch (ticketType) {
-            case ADULT:
-                netPrice = basePrice;
-                break;
-            case CHILD:
-                netPrice = basePrice * percentageDiscountChild / 100.0;
-                break;
-            case LUGGAGE:
-                netPrice = count * basePrice * percentageLuggage / 100.0;
-                break;
-            default:
-                netPrice = -1.0;
-        }
-
-        return netPrice;
+        return totalPrice;
     }
 
     public TicketType getTicketType() {
@@ -58,5 +40,21 @@ public class Ticket {
 
     public Integer getCount() {
         return count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return ticketType == ticket.ticketType &&
+                Objects.equals(basePrice, ticket.basePrice) &&
+                Objects.equals(count, ticket.count) &&
+                Objects.equals(totalPrice, ticket.totalPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticketType, basePrice, count, totalPrice);
     }
 }

83d76c68014095ff3d5aaff00339feaba60d437f
janissl <janis_slapins@inbox.lv>
2021-08-12 00:08:44 +0300

    Include customized error messages in the response body

--- src/main/java/com/github/janissl/draftticket/controller/DraftTicketController.java
+++ src/main/java/com/github/janissl/draftticket/controller/DraftTicketController.java
@@ -1,16 +1,19 @@
 package com.github.janissl.draftticket.controller;
 
+import com.github.janissl.draftticket.exception.DataUnavailableException;
 import com.github.janissl.draftticket.exception.ExternalServiceUnavailableException;
 import com.github.janissl.draftticket.exception.InvalidUserInputException;
 import com.github.janissl.draftticket.model.DraftPrice;
 import com.github.janissl.draftticket.model.Passenger;
 import com.github.janissl.draftticket.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 class DraftTicketController {
@@ -25,5 +28,32 @@ class DraftTicketController {
     public DraftPrice getDraftTicket(@RequestBody List<Passenger> passengerList)
             throws InvalidUserInputException, ExternalServiceUnavailableException {
         return priceService.getDraftPrice(passengerList);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(DataUnavailableException.class)
+    public ResponseEntity<Map<String, String>> returnDataUnavailableErrorMessage(DataUnavailableException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Data Unavailable");
+        errorResponse.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ExternalServiceUnavailableException.class)
+    public ResponseEntity<Map<String, String>> returnExternalServiceUnavailableErrorMessage(ExternalServiceUnavailableException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "External Service Unavailable");
+        errorResponse.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InvalidUserInputException.class)
+    public ResponseEntity<Map<String, String>> returnInvalidUserInputErrorMessage(InvalidUserInputException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Invalid Input From Client");
+        errorResponse.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
